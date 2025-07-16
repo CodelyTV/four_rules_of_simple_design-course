eedee11ba4ecd1b5d89f84d06b2b8ecbb7917cc0
Rafa Gómez <rgomezcasas@gmail.com>
2025-07-17 00:12:07 +0200

    feat(05-2): add example

--- 05-tests/2-reproduce_bug/tests/modules/users/infrastructure/InMemoryUserRepository.test.ts
+++ 05-tests/2-reproduce_bug/tests/modules/users/infrastructure/InMemoryUserRepository.test.ts
@@ -17,6 +17,18 @@ describe("InMemoryUserRepository", () => {
 		expect(savedUser).toBe(user);
 	});
 
+	it("should upsert a user", async () => {
+		const originalUser = new User("123", "original@example.com");
+		const updatedUser = new User("123", "updated@example.com");
+
+		await repository.save(originalUser);
+		await repository.save(updatedUser);
+
+		const savedUser = await repository.search("123");
+
+		expect(savedUser).toBe(updatedUser);
+	});
+
 	it("should return user when found", async () => {
 		const user = new User("123", "test@example.com");
 		await repository.save(user);

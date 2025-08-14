b41799a342e4b2099c5ae63fab34c1f82f322fc4
Rafa Gómez <rgomezcasas@gmail.com>
2025-08-14 10:42:12 +0200

    feat(05-1): add tests

--- 05-tests/1-how_to_refactor_wo_breaking_tests/tests/modules/users/application/register/UserRegistrar.fragile.test.ts
+++ 05-tests/1-how_to_refactor_wo_breaking_tests/tests/modules/users/application/register/UserRegistrar.fragile.test.ts
@@ -1,24 +1,44 @@
 /* eslint-disable */
-import { UserRegistrar } from "@/modules/users/application/register/UserRegistrar";
-import { User } from "@/modules/users/domain/User";
+import {
+	UserRegistrar
+} from "@/modules/users/application/register/UserRegistrar";
+import {User} from "@/modules/users/domain/User";
 
-import { MockUserRepository } from "../../infrastructure/MockUserRepository";
+import {MockUserRepository} from "../../infrastructure/MockUserRepository";
+import {
+	UserAlreadyExistsError
+} from "@/modules/users/domain/UserAlreadyExistsError";
 
 describe("UserRegistrar - FRAGILE TESTS", () => {
 	const repository = new MockUserRepository();
 	const userRegistrar = new UserRegistrar(repository);
+	const ensureUserSpy = jest
+		.spyOn(userRegistrar as any, "ensureUserDoesNotAlreadyExists")
+		.mockResolvedValue(undefined);
+
+	afterEach(() => {
+		ensureUserSpy.mockRestore();
+	});
 
 	it("should call ensureUserDoesNotAlreadyExists and save user when registering", async () => {
-		const ensureUserSpy = jest
-			.spyOn(userRegistrar as any, "ensureUserDoesNotAlreadyExists")
-			.mockResolvedValue(undefined);
-
 		repository.shouldSave(new User("123", "test@example.com"));
 
 		await userRegistrar.register("123", "test@example.com");
 
 		expect(ensureUserSpy).toHaveBeenCalledWith("test@example.com");
+	});
 
-		ensureUserSpy.mockRestore();
+	it("should not save user when ensureUserDoesNotAlreadyExists throws error", async () => {
+		const userEmail = "test@example.com";
+
+		const ensureUserSpy = jest
+			.spyOn(userRegistrar as any, "ensureUserDoesNotAlreadyExists")
+			.mockRejectedValue(new UserAlreadyExistsError(userEmail));
+
+		await expect(
+			userRegistrar.register("123", userEmail)
+		).rejects.toThrow(UserAlreadyExistsError);
+
+		expect(ensureUserSpy).toHaveBeenCalledWith(userEmail);
 	});
 });
